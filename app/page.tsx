3304ea62c50c36621d2e1e3ba035cfb067638ee9
Atul Kumar <atul.kumar@redblink.net>
2023-11-09 12:42:45 +0530

    create posts component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
 import CategoriesList from "@/components/CategoriesList";
+import Post from "@/components/Post";
+import { postsData } from "@/data";
 
 export default function Home() {
     return (
         <main>
             <CategoriesList />
+            {postsData && postsData.length > 0 ? (
+                postsData.map((post) => (
+                    <Post
+                        key={post.id}
+                        id={post.id}
+                        author={post.author}
+                        authorEmail={"test@demo.com"}
+                        date={post.datepublished}
+                        thumbnail={post.thumbnail}
+                        category={post.category}
+                        title={post.title}
+                        content={post.content}
+                        links={post.links || []}
+                    />
+                ))
+            ) : (
+                <div className="mt-10">No data found..!!</div>
+            )}
         </main>
     );
 }
--- data.js
+++ data.js
@@ -1,3 +1,124 @@
+export const postsData = [
+    {
+        id: 1,
+        title: "New Smartphone Model Released",
+        content:
+            "A new smartphone model with advanced features has been released.",
+        author: "John Doe",
+        datepublished: "2023-09-20",
+        category: "Technology",
+        links: ["https://example.com/news1"],
+        thumbnail:
+            "https://images.pexels.com/photos/1616516/pexels-photo-1616516.jpeg",
+    },
+    {
+        id: 2,
+        title: "Upcoming Tech Conference Announced",
+        content:
+            "An exciting tech conference showcasing the latest innovations is set to happen next month.",
+        author: "Jane Smith",
+        datepublished: "2023-10-05",
+        category: "Technology",
+        links: ["https://example.com/news2"],
+        thumbnail: "",
+    },
+    {
+        id: 3,
+        title: "AI Advancements Revolutionizing Industries",
+        content:
+            "Artificial Intelligence is making significant strides, transforming various industries with its capabilities.",
+        author: "Alex Johnson",
+        datepublished: "2023-10-15",
+        category: "AI",
+        links: ["https://example.com/news3"],
+        thumbnail: "",
+    },
+    {
+        id: 4,
+        title: "Green Technology: The Future of Sustainability",
+        content:
+            "Exploring the latest innovations in green tech that promise a sustainable future.",
+        author: "Emily Brown",
+        datepublished: "2023-10-25",
+        category: "Programming",
+        links: ["https://example.com/news4"],
+        thumbnail:
+            "https://images.pexels.com/photos/574073/pexels-photo-574073.jpeg",
+    },
+    {
+        id: 5,
+        title: "Space Exploration: New Discoveries Await",
+        content:
+            "Unraveling the mysteries of space with recent discoveries and upcoming missions.",
+        author: "Michael Davis",
+        datepublished: "2023-11-03",
+        category: "AI",
+        links: ["https://example.com/news5"],
+        thumbnail:
+            "https://images.pexels.com/photos/1181633/pexels-photo-1181633.jpeg",
+    },
+    {
+        id: 6,
+        title: "Advances in Biotechnology: Transforming Healthcare",
+        content:
+            "Biotech breakthroughs poised to revolutionize healthcare and medical treatments.",
+        author: "Sophia Wilson",
+        datepublished: "2023-11-10",
+        category: "Programming",
+        links: ["https://example.com/news6"],
+        thumbnail:
+            "https://images.pexels.com/photos/3770599/pexels-photo-3770599.jpeg",
+    },
+    {
+        id: 7,
+        title: "Future of Robotics: A Glimpse into Tomorrow",
+        content:
+            "Exploring the potential of robotics and its impact on various aspects of life.",
+        author: "William Clark",
+        datepublished: "2023-11-20",
+        category: "Tips & Tricks",
+        links: ["https://example.com/news7"],
+        thumbnail:
+            "https://images.pexels.com/photos/4344961/pexels-photo-4344961.jpeg",
+    },
+    {
+        id: 8,
+        title: "Blockchain Innovation Reshaping Industries",
+        content:
+            "How blockchain technology is revolutionizing sectors beyond cryptocurrency.",
+        author: "Olivia Martinez",
+        datepublished: "2023-11-30",
+        category: "Technology",
+        links: ["https://example.com/news8"],
+        thumbnail:
+            "https://images.pexels.com/photos/3750838/pexels-photo-3750838.jpeg",
+    },
+    {
+        id: 9,
+        title: "Renewable Energy: The Key to a Sustainable Future",
+        content:
+            "The latest breakthroughs and trends in renewable energy sources for a greener planet.",
+        author: "Daniel White",
+        datepublished: "2023-12-05",
+        category: "Tips & Tricks",
+        links: ["https://example.com/news9"],
+        thumbnail:
+            "https://images.pexels.com/photos/1148820/pexels-photo-1148820.jpeg",
+    },
+    {
+        id: 10,
+        title: "Cybersecurity: Protecting Data in the Digital Age",
+        content:
+            "The significance of cybersecurity and strategies to safeguard sensitive digital information.",
+        author: "Ava Thompson",
+        datepublished: "2023-12-15",
+        category: "AI",
+        links: ["https://example.com/news10"],
+        thumbnail:
+            "https://images.pexels.com/photos/430208/pexels-photo-430208.jpeg",
+    },
+];
+
 export const categoriesData = [
     { id: 1, name: "Technology" },
     { id: 2, name: "AI" },
