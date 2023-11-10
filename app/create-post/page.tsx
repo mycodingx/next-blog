1774eb95b8746a4d648b6475365870289db7d6c6
Atul Kumar <atul.kumar@redblink.net>
2023-11-10 09:52:32 +0530

    implement route protection

--- app/create-post/page.tsx
+++ app/create-post/page.tsx
@@ -1,9 +1,12 @@
 import CreatePostForm from "@/components/CreatePostForm";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function CreatePost() {
-    return (
-        <div>
-            <CreatePostForm />
-        </div>
-    );
+export default async function CreatePost() {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        redirect("/sign-in");
+    }
+    return <CreatePostForm />;
 }
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,9 +1,12 @@
 import SignInBtns from "@/components/SignInBtns";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function SignIn() {
-    return (
-        <div>
-            <SignInBtns />
-        </div>
-    );
+export default async function SignIn() {
+    const session = await getServerSession(authOptions);
+    if (session) {
+        redirect("/dashboard");
+    }
+    return <SignInBtns />;
 }
