ebc7c8c815a3686c501fbfb986f84aede2cfd9f4
Atul Kumar <atul.kumar@redblink.net>
2023-11-13 18:11:16 +0530

    connected categorylist to category api

--- components/CategoriesList.tsx
+++ components/CategoriesList.tsx
@@ -1,17 +1,30 @@
-import { categoriesData } from "@/data";
+// import { categoriesData } from "@/data"; Moving form file to db
+import { TCategory } from "@/app/types";
 import Link from "next/link";
 
-export default function CategoriesList() {
+const getCategories = async () => {
+    try {
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/categories`);
+        const categories = await res.json();
+        return categories;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+};
+
+export default async function CategoriesList() {
+    const categories = await getCategories();
     return (
         <div className="flex gap-2 text-sm flex-wrap">
-            {categoriesData &&
-                categoriesData.map((category) => (
+            {categories &&
+                categories.map((category: TCategory) => (
                     <Link
                         className="px-4 py-1 rounded-md bg-primary text-white cursor-pointer"
                         key={category.id}
-                        href={`/categories/${category.name}`}
+                        href={`/categories/${category.catName}`}
                     >
-                        {category.name}
+                        {category.catName}
                     </Link>
                 ))}
         </div>
